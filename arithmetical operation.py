a6be7f3d5a225a585d64b478f00034d1ef1b43cf
DHRUVa-MISHRa <r.dhruva.s168@gmail.com>
2025-06-10 15:54:00 +0530

    Add files via upload

---
+++ arithmetical operation.py
@@ -0,0 +1,15 @@
+a=int(input("enter first no."))
+b=int(input("enter second no."))
+print("selet your arithmetical operation:")
+print("1.addition 2.subtaction 3.multipliaction 4.division\n")
+ch=int(input("type your option:"))
+if(ch==1):
+    print(a+b)
+if(ch==2):
+    print(a-b)
+if(ch==3):
+    print(a*b)
+if(ch==4):
+    print(a/b)
+    
+ 
